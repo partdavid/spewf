e6f771eb0f5e3e2a34b731744c562cf19793ab35
partdavid <partdavid@dbcf5056-9645-0410-a7e2-3920ef285e7c>
2008-03-19 04:04:07 +0000

    fixing up scaffolding

--- lib/board/src/board.erl
+++ lib/board/src/board.erl
@@ -10,10 +10,17 @@
 -behaviour(spewf_session).
 
 -include_lib("eunit/include/eunit.hrl").
+-include_lib("yaws/include/yaws_api.hrl").
 
 -export([init/1,
-         handle_request/2]).
+         handle_request/2,
+         out/1]).
 
+%% ----------------------------------------------
+%% spewf_session callbacks
+%% These functions implement stateful session
+%% logic using SPEWF callbacks
+%% ----------------------------------------------
 -record(state, {said = ""}).
 
 init([]) ->
@@ -29,14 +36,14 @@ handle_request(R, S) ->
              {value, {said, Value}} -> Value;
              false -> "You said nothing"
           end,
-   Reply = {ehtml, [{h2, [], "BOARD Application"},
-                    {p, [], ["Before: ", S#state.said]},
-                    {p, [], ["This time: ", Said]},
-                    {form, [{method, get}],
-                     [{input, [{type, "text"}, {name, "said"}, {size, 50}], []},
-                      {input, [{type, "submit"}], []}]},
-                    show_req(R)
-                   ]},
+   Reply = {spewf, [], [{h2, [], "BOARD Application"},
+                        {p, [], ["Before: ", S#state.said]},
+                        {p, [], ["This time: ", Said]},
+                        {form, [{method, post}],
+                         [{input, [{type, "text"}, {name, "said"}, {size, 50}], []},
+                          {input, [{type, "submit"}], []}]},
+                        show_req(R)
+                       ]},
    {Reply, #state{said = Said}}.
 
 show_req(R) ->
@@ -51,3 +58,16 @@ show_req([{K, V}|R], A) ->
                   {td, [{align, "left"}], [{b, [], Ks}, ":"]},
                   {td, [{align, "left"}], io_lib:format("~p", [V])}
                  ]}|A]).
+
+%% ---------------------------------------
+%% Yaws callback
+%% These functions support yaws callbacks,
+%% that is, stateless views of the application
+%% ---------------------------------------
+
+out(A) ->
+   R = spewf:make_req(A),
+   spewf_lang:trans({spewf, [{subapp, ?MODULE}],
+                     [{h2, "BOARD Application"},
+                      {p, "Stateless page"},
+                      show_req(R)]}).
